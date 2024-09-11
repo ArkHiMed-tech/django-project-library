e1398d6339e62492bc59e61b9212156c6b29e320
ArkHiMed-tech <boss.trushchelev@mail.ru>
2024-09-11 17:44:47 +0300

    final changes

--- library/books/urls.py
+++ library/books/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("lib/<int:library_id>", views.library_api, name="library"),
+    path("book/<str:book_name>", views.book_api, name="book"),
 ]
--- library/books/views.py
+++ library/books/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from .models import Library, Book
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def library_api(request, library_id):
+    try:
+        return JsonResponse(model_to_dict(Library.objects.get(id=library_id)))
+    except Library.DoesNotExist:
+        return JsonResponse('Lib not found', safe=False)
+    
+def book_api(request, book_name):
+    try:
+        return JsonResponse(model_to_dict(Book.objects.get(name=book_name)))
+    except Book.DoesNotExist:
+        return JsonResponse('Book not found', safe=False)
